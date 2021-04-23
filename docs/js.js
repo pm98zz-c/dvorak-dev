1edf85cfa6ebf9336da8e7a53f915bfc9851a86f
pascal <pascal.morin@codeenigma.com>
2021-04-23 10:45:03 +0200

    draft

---
+++ docs/js.js
@@ -0,0 +1,79 @@
+let currentLevel = Number(localStorage.getItem('currentLevel')) || 1
+const searchParams = new URLSearchParams(location.search)
+const currentLevelParam = parseInt(searchParams.get("level"))
+if (currentLevelParam > 0 && currentLevelParam < 6) {
+  currentLevel = currentLevelParam
+}
+localStorage.setItem('currentLevel', currentLevel)
+document.getElementById('results').innerHTML = 'Level ' + currentLevel
+const template = document.getElementById('template')
+const playground = document.getElementById('playground')
+const train = document.getElementById('train')
+const level1 = 'aseuhton'
+const level2 = 'qjkmwv'
+const level3 = 'idypgcrl'
+const level4 = 'xzf'
+const levels = [level1, level2, level3, level4, '']
+const wordsList = 'axiomatize deoxidized deoxidizer deoxidizes epoxidized epoxidizes exahertzes exoenzymes exorcizing expertized expertizes lexicalize maximizers maximizing oxidizable oxygenized oxygenizes reoxidized reoxidizes sexualized sexualizes texturizes Requirement 1b: Password Length Enforcement on Change Requirement 1c: Password Length Enforcement on Login return getElementFromSelector(element) || element.closest(`.${CLASSNAME_ALERT}`) Alert.handleDismiss(new Alert())) namespace PHPMailer\PHPMailer; use Hayageek\OAuth2\Client\Provider\Yahoo; require \'vendor/autoload.php\'; <?php $redirectUri = (isset($_SERVER[\'HTTPS\']) ? \'https://\' : \'http://\') . $_SERVER[\'HTTP_HOST\'] . $_SERVER[\'PHP_SELF\']; switch ($providerName) { if (!isset($_GET[\'code\'])) { // If we don\'t have an authorization code then get one $authUrl = $provider->getAuthorizationUrl($options); $_SESSION[\'oauth2state\'] = $provider->getState(); // Check given state against previously stored one to mitigate CSRF attack } elseif (empty($_GET[\'state\']) || ($_GET[\'state\'] !== $_SESSION[\'oauth2state\'])) { unset($_SESSION[\'oauth2state\']); unset($_SESSION[\'provider\']); exit(\'Invalid state\'); $token = $provider->getAccessToken( the of to and a in is it you that he was for on are with as I his they be at one have this from or had by not word but what some we can out other were all there when up use your how said an each she which do their time if will way about many then them write would like so these her long make thing see him two has look more day could go come did number sound no most people my over know water than call first who may down side been now find any new work part take get place made live where after back little only round man year came show every good me give our under name very through just form sentence great think say help low line differ turn cause much mean before move right boy old too same tell does set three want air well also play small end put home read hand port large spell add even land here must big high such follow act why ask men change went light kind off need house picture try us again animal point mother world near build self earth father head stand own page should country found answer school grow study still learn plant cover food sun four between state keep eye never last let thought city tree cross farm hard start might story saw far sea draw left late run don\'t while press close night real life few north open seem together next white children begin got walk example ease paper group always music those both mark often letter until mile river car feet care second book carry took science eat room friend began idea fish mountain stop once base hear horse cut sure watch color face wood main enough plain girl usual young ready above ever red list though feel talk bird soon body dog family direct pose leave song measure door product black short numeral class wind question happen complete ship area half rock order fire south problem piece told knew pass since top whole king space heard best hour better true during hundred five remember step early hold west ground interest reach fast verb sing listen six table travel less morning ten simple several vowel toward war lay against pattern slow center love person money serve appear road map rain rule govern pull cold notice voice unit power town fine certain fly fall lead cry dark machine note wait plan figure star box noun field rest correct able pound done beauty drive stood contain front teach week final gave green oh quick develop ocean warm free minute strong special mind behind clear tail produce fact street inch multiply nothing course stay wheel full force blue object decide surface deep moon island foot system busy test record boat common gold possible plane stead dry wonder laugh thousand ago ran check game shape equate hot miss brought heat snow tire bring yes distant fill east paint language among grand ball yet wave drop heart am present heavy dance engine position arm wide sail material size vary settle speak weight general ice matter circle pair include divide syllable felt perhaps pick sudden count square reason length represent art subject region energy hunt probable bed brother egg ride cell believe fraction forest sit race window store summer train sleep prove lone leg exercise wall catch mount wish sky board joy winter sat written wild instrument kept glass grass cow job edge sign visit past soft fun bright gas weather month million bear finish happy hope flower clothe strange gone jump baby eight village meet root buy raise solve metal whether push seven paragraph third shall held hair describe cook floor either result burn hill safe cat century consider type law bit coast copy phrase silent tall sand soil roll temperature finger industry value fight lie beat excite natural view sense ear else quite broke case middle kill son lake moment scale loud spring observe child straight consonant nation dictionary milk speed method organ pay age section dress cloud surprise quiet stone tiny climb cool design poor lot experiment bottom key iron single stick flat twenty skin smile crease hole trade melody trip office receive row mouth exact symbol die least trouble shout except wrote seed tone join suggest clean break lady yard rise bad blow oil blood touch grew cent mix team wire cost lost brown wear garden equal sent choose fell fit flow fair bank collect save control decimal gentle woman captain practice separate difficult doctor please protect noon whose locate ring character insect caught period indicate radio spoke atom human history effect electric expect crop modern element hit student corner party supply bone rail imagine provide agree thus capital won\'t chair danger fruit rich thick soldier process operate guess necessary sharp wing create neighbor wash bat rather crowd corn compare poem string bell depend meat rub tube famous dollar stream fear sight thin triangle planet hurry chief colony clock mine tie enter major fresh search send yellow gun allow print dead spot desert suit current lift rose continue block chart hat sell success company subtract event particular deal swim term opposite wife shoe shoulder spread arrange camp invent cotton born determine quart nine truck noise level chance gather shop stretch throw shine property column molecule select wrong gray repeat require broad prepare salt nose plural anger claim continent oxygen sugar death pretty skill women season solution magnet silver thank branch match suffix especially fig afraid huge sister steel discuss forward similar guide experience score apple bought led pitch coat mass card band rope slip win dream evening condition feed tool total basic smell valley nor double seat arrive master track parent shore division sheet substance favor connect post spend chord fat glad original share station dad bread charge proper bar offer segment slave duck instant market degree populate chick dear enemy reply drink occur support speech nature range steam motion path liquid log meant quotient teeth shell neck show databases; mysql --defaults-extra-file=/home/deploy/.mysql.credsapply from: "gradle/dependencies.gradle" allprojects { group = \'com.example.gdx\' version = \'1.9.12-SNAPSHOT\' buildscript { repositories { google() jcenter() mavenCentral() maven { url "https://oss.sonatype.org/content/repositories/snapshots/" } } } repositories { google() jcenter() mavenCentral() maven { url "https://oss.example.org/content/repositories/snapshots/" } }}configure(subprojects - project(":tests:gdx-tests-android")) { apply plugin: "idea" apply plugin: "java" compileJava { options.fork = options.incremental = }}ant.importBuild("fetch.xml") { antTargetName -> "ant-task-" + antTargetName }task fetchNatives(dependsOn: "ant-task-all"){ doLast { }}from __future__ import (absolute_import, division, print_function)__metaclass__ = typefrom ansible import constants as Cfrom ansible import contextfrom ansible.cli import CLIfrom ansible.cli.arguments import option_helpers as opt_helpfrom ansible.errors import AnsibleError, AnsibleOptionsErrorfrom ansible.executor.task_queue_manager import TaskQueueManagerfrom ansible.module_utils._text import to_textfrom ansible.parsing.splitter import parse_kvfrom ansible.playbook import Playbookdef init_parser(self):opt_help.add_connect_options(self.parser)if context.CLIARGS[\'module_name\'] not in (\'include_role\', \'include_tasks\') and any(frozenset((async_val, poll))): mkdir empty && rsync -r --delete empty/ some-dir && rmdir some-dir diff <(jq --sort-keys . < file1.json) <(jq --sort-keys . < file2.json) | colordiff | less -R awk \'{ x += $3 } END { print x }\' myfile gpg --keyserver hkp://keys.gnupg.net --receive-keys .carousel { position: relative; } .carousel.pointer-event { touch-action: pan-y; } .carousel-inner { position: relative; width: 100%; overflow: hidden; @include clearfix(); } .carousel-item { position: relative; display: none; float: left; width: 100%; margin-right: -100%; backface-visibility: hidden; @include transition($carousel-transition); } import { getjQuery, TRANSITION_END, emulateTransitionEnd, getElementFromSelector, getTransitionDurationFromElement } from \'./util/index\' import Data from \'./dom/data\' import EventHandler from \'./dom/event-handler\' const NAME = \'alert\' const VERSION = \'5.0.0-alpha2\' const DATA_KEY = \'bs.alert\' const EVENT_KEY = `.${DATA_KEY}` const DATA_API_KEY = \'.data-api\' class Alert { constructor(element) { this._element = element if (this._element) { Data.setData(element, DATA_KEY, this) } } close(element) { const rootElement = element ? this._getRootElement(element) : this._element const customEvent = this._triggerCloseEvent(rootElement) if (customEvent === null || customEvent.defaultPrevented)'
+const words = wordsList.split(' ')
+let filtered = words
+if (levels[currentLevel - 1].length) {
+  let match = ''
+  for (let i = currentLevel - 1; i >= 0; i--) {
+    match += levels[i]
+  }
+  const filter = new RegExp('[^' + match + ']')
+  const contains = new RegExp('[' + levels[currentLevel - 1] + ']')
+  filtered = words.filter((word) => {
+    if (word.match(filter) === null) {
+      return word.match(contains) !== null
+    }
+    return false
+  })
+} else {
+  const contains = new RegExp('[^a-z]')
+  filtered = words.filter((word) => {
+    return word.match(contains) !== null
+  })
+}
+let shuffled = []
+for (let i = 20; i > 0; i--) {
+  const j = Math.floor(Math.random() * filtered.length)
+  shuffled.push(filtered[j])
+}
+localStorage.setItem('errors', 0)
+const initial = shuffled.join(' ')
+template.value = initial
+template.style.height = template.scrollHeight + "px"
+playground.style.height = template.scrollHeight + "px"
+const accuracy = document.getElementById('accuracy')
+playground.value = ''
+let currentText = playground.value
+playground.onkeydown = ((e) => {
+  localStorage.setItem('currentText', playground.value)
+})
+let errorCount = 0
+playground.onkeyup = ((e) => {
+  let typedText = playground.value
+  if (template.value.indexOf(typedText) !== 0) {
+    playground.value = localStorage.getItem('currentText')
+    errorCount++
+    train.classList.remove('valid')
+    train.classList.add('invalid')
+  } else {
+    playground.classList.remove('invalid')
+    if (playground.value.length > 0) {
+      train.classList.add('valid')
+    }
+  }
+  template.scrollLeft = playground.scrollLeft
+  accuracy.innerHTML = playground.value.length + '/' + initial.length + ' (' + errorCount + ' errors)'
+  if (playground.value.length === initial.length) {
+    playground.setAttribute('readonly', 'readonly')
+    let message = '<span>WELL DONE! </span><a href="?level=' + currentLevel + '">Practice again</a>'
+    if (currentLevel < 5) {
+      message += ' or <a href="?level=' + (Number(currentLevel) + 1) + '">move to the next level</a>?'
+    }
+    document.getElementById('results').innerHTML = message
+  }
+})
